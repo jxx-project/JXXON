60f434544946c683f34f9f7aa4d9675dc7b39320
Michael Steffens <michael_steffens@posteo.de>
2018-10-29 14:13:16 +0100

    Add UnorderedMultiset type.

---
+++ test/unorderedMultisetTest.cpp
@@ -0,0 +1,14 @@
+//
+// Copyright (C) 2018 Dr. Michael Steffens
+//
+// SPDX-License-Identifier:	BSL-1.0
+//
+
+
+#include "JXXON/UnorderedMultiset.h"
+#include "Testee/setTest.h"
+
+int main(int argc, char* argv[])
+{
+	return Testee::setTest<JXXON::UnorderedMultiset>("JXXON::UnorderedMultiset");
+}
